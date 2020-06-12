0df143be8630a833ed34d87b55e7e621f1c8377b
KOUAM Ledoux <kouamdoux@gmail.com>
2020-06-13 01:58:47 +0200

    small testing framework

--- src/test/test.c
+++ src/test/test.c
@@ -3,17 +3,17 @@
 #include <stddef.h>
 #include <stdint.h>
 
-test_case_result TEST_SUBSYSTEM[0XFF];
-uint16_t NMBER_TEST_CASE = 0;
+static test_case_result test_table_[0XFF];
+static uint16_t nmber_test_ = 0;
 
 uint16_t find_test_case(char* test_case)
 {
-    if (NMBER_TEST_CASE)
+    if (nmber_test_)
         return 0;
 
     uint8_t i = 0;
-    for (i; i < NMBER_TEST_CASE; i++)
-        if (_strcmp_(test_case, TEST_SUBSYSTEM[i].test_case_name) == true)
+    for (i; i < nmber_test_; i++)
+        if (_strcmp_(test_case, test_table_[i].test_case_name) == true)
             return (uint16_t)i;
 
     return (uint16_t)(-1);
@@ -37,21 +37,20 @@ void test_unit(char _test_case_[0xF], char _test_unit[0xF], void* test_function)
 {
     uint16_t test_case_index = find_test_case(_test_case_);
 
-    if (NMBER_TEST_CASE == 0)
+    if (nmber_test_ == 0)
         return;
 
     if (test_case_index == (uint16_t)(-1)) // If the test case doesn't exist
         return;
 
-    uint16_t test_unit_index =
-        find_unit_test(_test_unit, TEST_SUBSYSTEM[test_case_index]);
+    uint16_t test_unit_index = find_unit_test(_test_unit, test_table_[test_case_index]);
 
-    if (TEST_SUBSYSTEM[test_case_index].nmber_test != 0) // Verify the zero value of test_unit_index
+    if (test_table_[test_case_index].nmber_test != 0) // Verify the zero value of test_unit_index
         if (test_unit_index != (uint16_t)(-1))
             return;
 
     test_case_result tmp;
-    tmp = TEST_SUBSYSTEM[test_case_index]; // Check the test case
+    tmp = test_table_[test_case_index]; // Check the test case
     test_unit_result* tmp_test_unit;
     tmp_test_unit = (&(tmp.tests_units[tmp.nmber_test])); // check the unit test a the end
 
@@ -67,7 +66,7 @@ void test_unit(char _test_case_[0xF], char _test_unit[0xF], void* test_function)
 
     // Save the unit test
     tmp.nmber_test++;
-    TEST_SUBSYSTEM[find_test_case(_test_case_)] = tmp;
+    test_table_[find_test_case(_test_case_)] = tmp;
 }
 
 void test_unit_throws_valid_test(char _test_case_[0xF],
@@ -79,16 +78,16 @@ void test_unit_throws_valid_test(char _test_case_[0xF],
 {
     uint16_t test_case_index = find_test_case(_test_case_result);
 
-    if (NMBER_TEST_CASE == 0)
+    if (nmber_test_ == 0)
         return;
 
     if (test_case_index == (uint16_t)(-1)) // If the test case doesn't exist
         return;
 
     uint16_t test_unit_index =
-        find_unit_test(_test_unit_result, TEST_SUBSYSTEM[test_case_index]);
+        find_unit_test(_test_unit_result, test_table_[test_case_index]);
 
-    if (TEST_SUBSYSTEM[test_case_index].nmber_test == 0)
+    if (test_table_[test_case_index].nmber_test == 0)
         return;
 
     if (test_unit_index == (uint16_t)(-1))
@@ -98,13 +97,13 @@ void test_unit_throws_valid_test(char _test_case_[0xF],
     test_unit(_test_case_, _test_unit, test_function);
 
     // Insert function result
-    _strcpy_(TEST_SUBSYSTEM[find_test_case(_test_case_)]
-                 .tests_units[find_unit_test(_test_unit, TEST_SUBSYSTEM[test_case_index])]
+    _strcpy_(test_table_[find_test_case(_test_case_)]
+                 .tests_units[find_unit_test(_test_unit, test_table_[test_case_index])]
                  .valid_test.test_unit,
              _test_unit_result);
 
-    _strcpy_(TEST_SUBSYSTEM[find_test_case(_test_case_)]
-                 .tests_units[find_unit_test(_test_unit, TEST_SUBSYSTEM[test_case_index])]
+    _strcpy_(test_table_[find_test_case(_test_case_)]
+                 .tests_units[find_unit_test(_test_unit, test_table_[test_case_index])]
                  .valid_test.test_case,
              _test_unit_result);
 }
@@ -118,16 +117,16 @@ void test_unit_throws_unvalid_test(char _test_case_[0xF],
 {
     uint16_t test_case_index = find_test_case(_test_case_result);
 
-    if (NMBER_TEST_CASE == 0)
+    if (nmber_test_ == 0)
         return;
 
     if (test_case_index == (uint16_t)(-1)) // If the test case doesn't exist
         return;
 
     uint16_t test_unit_index =
-        find_unit_test(_test_unit_result, TEST_SUBSYSTEM[test_case_index]);
+        find_unit_test(_test_unit_result, test_table_[test_case_index]);
 
-    if (TEST_SUBSYSTEM[test_case_index].nmber_test == 0)
+    if (test_table_[test_case_index].nmber_test == 0)
         return;
 
     if (test_unit_index == (uint16_t)(-1))
@@ -137,20 +136,24 @@ void test_unit_throws_unvalid_test(char _test_case_[0xF],
     test_unit(_test_case_, _test_unit, test_function);
 
     // Insert function result
-    _strcpy_(TEST_SUBSYSTEM[find_test_case(_test_case_)]
-                 .tests_units[find_unit_test(_test_unit, TEST_SUBSYSTEM[test_case_index])]
+    _strcpy_(test_table_[find_test_case(_test_case_)]
+                 .tests_units[find_unit_test(_test_unit, test_table_[test_case_index])]
                  .unvalid_test.test_unit,
              _test_unit_result);
 
-    _strcpy_(TEST_SUBSYSTEM[find_test_case(_test_case_)]
-                 .tests_units[find_unit_test(_test_unit, TEST_SUBSYSTEM[test_case_index])]
+    _strcpy_(test_table_[find_test_case(_test_case_)]
+                 .tests_units[find_unit_test(_test_unit, test_table_[test_case_index])]
                  .unvalid_test.test_case,
              _test_unit_result);
 }
 
 void test_case(char* _test_case_)
 {
-    _strcpy_(TEST_SUBSYSTEM[NMBER_TEST_CASE].test_case_name, _test_case_);
-    TEST_SUBSYSTEM[NMBER_TEST_CASE].nmber_test = 0;
-    NMBER_TEST_CASE++;
+    // Find if the test case exist on the table
+    if (nmber_test_ != 0 && find_test_case(_test_case_) != (uint16_t)(-1))
+        return;
+
+    _strcpy_(test_table_[nmber_test_].test_case_name, _test_case_);
+    test_table_[nmber_test_].nmber_test = 0;
+    nmber_test_++;
 }
